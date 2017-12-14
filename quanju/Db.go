923c6ed08b2d76c755a6cead06e0ac2b07cc7a8e
changlz <changlizhi@aliyun.com>
2017-12-14 19:45:37 +0800

    增加全局变量

---
+++ quanju/Db.go
@@ -0,0 +1,10 @@
+package quanju
+
+import (
+	"github.com/jinzhu/gorm"
+	"mhsyquanxian/gongjus"
+)
+
+var Db, _ = gorm.Open("mysql", "root:root@tcp(127.0.0.1:3306)/mhsyshuju?charset=utf8&parseTime=True&loc=Local")
+var Idworker, _ = gongjus.NewIdWorker()
+
