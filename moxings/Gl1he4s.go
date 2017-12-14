4e30ff1a3c171b7d6a43269da75fc07ae8bdc704
changlz <changlizhi@aliyun.com>
2017-12-14 21:00:54 +0800

    拼错改正

--- moxings/Gl1he4s.go
+++ moxings/Gl1he4s.go
@@ -3,7 +3,7 @@ package moxings
 type Gl1he4s struct {
 	Id          int64
 	Biaobianma1 string `gorm:"not null"`
-	Biaobainam4 string `gorm:"not null"`
+	Biaobianma4 string `gorm:"not null"`
 }
 
 func (Gl1he4s) TableName() string {
