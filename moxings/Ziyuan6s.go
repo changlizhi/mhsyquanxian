3a4ef9c74c56ea824fc789e09d560c237c9cc842
changlz <changlizhi@aliyun.com>
2017-12-14 21:29:28 +0800

    修改字段

--- moxings/Ziyuan6s.go
+++ moxings/Ziyuan6s.go
@@ -1,12 +1,14 @@
 package moxings
 
 type Ziyuan6s struct {
-	Id           int64
-	Mingcheng    string `gorm:"not null"`
-	Lujing       string `gorm:"not null"`
-	Ziyuanbianma string `gorm:"not null"`
-	Ziyuanshuzi  int64  `gorm:"not null"`
-	Miaoshu      string `gorm:"not null;DEFAULT:0"`
+	Id         int64
+	Biaobianma string `gorm:"not null"`
+	Bianma     string `gorm:"not null"`
+	Biaoshuzi  int64  `gorm:"not null"`
+	Mingcheng  string `gorm:"not null"`
+	Lujing     string `gorm:"not null"`
+	Fangfa     string `gorm:"not null"`//提交、删除
+	Miaoshu    string `gorm:"not null;DEFAULT:0"`
 }
 
 func (Ziyuan6s) TableName() string {
