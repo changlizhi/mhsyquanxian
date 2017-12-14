c6c26d145044ccc29ace8ce38dc09a400f4c13c3
changlz <changlizhi@aliyun.com>
2017-12-14 19:45:21 +0800

    增加工具方法

---
+++ gongjus/Yewujiegou.go
@@ -0,0 +1,46 @@
+package gongjus
+
+type Bangdingxinxi struct {
+	Openid    string
+	Shebeihao string
+}
+type Richangxinxi struct {
+	Openid     string
+	Tizhong    string
+	Xuetang    string
+	Zaoxuetang string
+	Wuxuetang  string
+	Wanxuetang string
+	Beizhu     string
+}
+type Yinpinxinxi struct {
+	Openid           string
+	Xueya            string
+	Xuetang          string
+	Zaoxuetang       string
+	Wuxuetang        string
+	Wanxuetang       string
+	Ganyousanzhi     string
+	Gaomiduzhidanbai string
+	Tizhong          string
+	Shengao          string
+	Xueniaosuan      string
+	Beizhu           string
+}
+type Yonghuxinxi struct {
+	Openid    string
+	Nianling  string
+	Dizhi     string
+	Xingbie   string
+	Tijian    string
+	Qidong    string
+	Beizhu    string
+	Xingming  string
+	Email     string
+	Shoujihao string
+}
+
+type Fanhuixinxi struct {
+	Biaoti       string
+	Xinxineirong string
+}
