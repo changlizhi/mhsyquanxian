d1b956edfe7ffd296b5826d8d0fa76215b0b4c4a
changlz <changlizhi@aliyun.com>
2017-12-14 20:23:37 +0800

    拆出flakeid的全局方法

--- quanju/Db.go
+++ quanju/Db.go
@@ -2,27 +2,21 @@ package quanju
 
 import (
 	"github.com/jinzhu/gorm"
-	"mhsyquanxian/gongjus"
 	_ "github.com/go-sql-driver/mysql"
 	"log"
 )
 
+var db *gorm.DB
+
 func Db() *gorm.DB {
-	Db, err := gorm.Open("mysql", "root:root@tcp(127.0.0.1:3306)/mhsyquanxian?charset=utf8&parseTime=True&loc=Local")
-	if err != nil {
-		log.Println("kaiqi shujuku shibai ---", err)
+	if db == nil {
+		log.Println(111)
+		db, err := gorm.Open("mysql", "root:root@tcp(127.0.0.1:3306)/mhsyquanxian?charset=utf8&parseTime=True&loc=Local")
+		if err != nil {
+			log.Println("kaiqi shujuku shibai ---", err)
+		}
+		return db
 	}
-	log.Println("Shujuku111-----", Db)
-	return Db
-}
-
-func FlakeId() *gongjus.IdWorker {
-	log.Println(111)
-
-	Flakeid, err := gongjus.NewIdWorker()
-	if err != nil {
-		log.Println("flakeid shengcheng shibai", err)
-	}
-	log.Println("Flakeid-----", Flakeid)
-	return Flakeid
+	log.Println("Shujuku111-----", db)
+	return db
 }
