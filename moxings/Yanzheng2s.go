4bd932ab6c9cf671446cb9f10cb652eed03ffe51
changlz <changlizhi@aliyun.com>
2017-12-14 21:01:26 +0800

    增加策略方法

--- moxings/Yanzheng2s.go
+++ moxings/Yanzheng2s.go
@@ -1,11 +1,12 @@
 package moxings
 
 type Yanzheng2s struct {
-	Id         int64
-	Biaobianma string `gorm:"not null"`
-	Bianma     string `gorm:"not null"`
-	Mingcheng  string `gorm:"not null"`
-	Celue      string `gorm:"not null"`
+	Id          int64
+	Biaobianma  string `gorm:"not null"`
+	Bianma      string `gorm:"not null"`
+	Mingcheng   string `gorm:"not null"`
+	Celue       string `gorm:"not null"`
+	Celuefangfa string `gorm:"not null"`
 }
 
 func (Yanzheng2s) TableName() string {
