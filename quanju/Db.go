8889187be31b9efa6d2c255fb8375079e6fd3f81
changlz <changlizhi@aliyun.com>
2017-12-14 20:15:46 +0800

    提供方法调用而非全局变量

--- quanju/Db.go
+++ quanju/Db.go
@@ -3,8 +3,26 @@ package quanju
 import (
 	"github.com/jinzhu/gorm"
 	"mhsyquanxian/gongjus"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
 )
 
-var Db, _ = gorm.Open("mysql", "root:root@tcp(127.0.0.1:3306)/mhsyquanxian?charset=utf8&parseTime=True&loc=Local")
-var Idworker, _ = gongjus.NewIdWorker()
+func Db() *gorm.DB {
+	Db, err := gorm.Open("mysql", "root:root@tcp(127.0.0.1:3306)/mhsyquanxian?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		log.Println("kaiqi shujuku shibai ---", err)
+	}
+	log.Println("Shujuku111-----", Db)
+	return Db
+}
 
+func FlakeId() *gongjus.IdWorker {
+	log.Println(111)
+
+	Flakeid, err := gongjus.NewIdWorker()
+	if err != nil {
+		log.Println("flakeid shengcheng shibai", err)
+	}
+	log.Println("Flakeid-----", Flakeid)
+	return Flakeid
+}
