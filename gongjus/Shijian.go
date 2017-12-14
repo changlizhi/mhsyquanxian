0077eaee4b67fdc6d9b423b834664387a196a898
changlz <changlizhi@aliyun.com>
2017-12-14 21:28:59 +0800

    fmt

--- gongjus/Shijian.go
+++ gongjus/Shijian.go
@@ -16,8 +16,8 @@ func String2Float64(str string) float64 {
 //str必须为 yyyy-MM-dd hh:mm:ss
 func String2timenyrsfm(str string) time.Time {
 	//获取本地location
-	timeLayout := "2006-01-02 15:04:05"                             //转化所需模板
-	loc, _ := time.LoadLocation("Local")                            //重要：获取时区
+	timeLayout := "2006-01-02 15:04:05"                      //转化所需模板
+	loc, _ := time.LoadLocation("Local")                     //重要：获取时区
 	theTime, _ := time.ParseInLocation(timeLayout, str, loc) //使用模板在对应时区转化为time.time类型
 	//sr := theTime.Unix()                                            //转化为时间戳 类型是int64
 	//fmt.Println(theTime)                                            //打印输出theTime 2015-01-01 15:15:00 +0800 CST
@@ -28,10 +28,11 @@ func String2timenyrsfm(str string) time.Time {
 	//fmt.Println(dataTimeStr)
 	return theTime
 }
+
 //str必须为 yyyy-MM-dd
 func String2timenyr(str string) time.Time {
-	timeLayout := "2006-01-02"                             //转化所需模板
-	loc, _ := time.LoadLocation("Local")                            //重要：获取时区
+	timeLayout := "2006-01-02"                               //转化所需模板
+	loc, _ := time.LoadLocation("Local")                     //重要：获取时区
 	theTime, _ := time.ParseInLocation(timeLayout, str, loc) //使用模板在对应时区转化为time.time类型
 	return theTime
 }
