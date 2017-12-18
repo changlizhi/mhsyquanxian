95ec56a32e1b34525199bc91f94efe128e2c84ba
changlz <changlizhi@aliyun.com>
2017-12-19 00:08:54 +0800

    增加路由访问字段

---
+++ Main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"io"
+	"net/http"
+	"os"
+	"mhsyquanxian/ziduan7skus"
+	"mhsyquanxian/moxings"
+)
+
+type Retstruct struct {
+	Mingcheng string
+	Neirong   string
+}
+
+func Cors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Add("Access-Control-Allow-Origin", "*")
+		c.Next()
+	}
+}
+func main() {
+
+	gin.DisableConsoleColor()
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f)
+	r := gin.Default()
+	r.Use(Cors())
+	ziduan7s := r.Group("ziduan7s")
+	{
+		ziduan7s.GET("/:Biaobianma", func(c *gin.Context) {
+			//获取到数据之后第一步，log下来
+			//第二步验证授权
+			//第三步验证字段是否合法
+			//第四步拼接参数
+			//第五步访问业务
+			//第六步返回
+
+			bbm := c.Param("Biaobianma")
+			mx := moxings.Ziduan7s{
+				Biaobianma:bbm,
+			}
+			ret := ziduan7skus.ChaxunZiduan7s(mx)
+			c.JSON(http.StatusOK, ret)
+		})
+	}
+	r.Run(":8787")
+}
