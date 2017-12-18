14c78c3416a3192bab875067a36b2137205b507a
changlz <changlizhi@aliyun.com>
2017-12-18 10:15:02 +0800

    fmt

--- moxings/Juese5s.go
+++ moxings/Juese5s.go
@@ -1,11 +1,11 @@
 package moxings
 
 type Juese5s struct {
-	Id          int64
+	Id         int64
 	Biaobianma string `gorm:"not null"`
-	Bianma string `gorm:"not null"`
-	Mingcheng   string `gorm:"not null"`
-	Miaoshu     string `gorm:"not null;DEFAULT:0"`
+	Bianma     string `gorm:"not null"`
+	Mingcheng  string `gorm:"not null"`
+	Miaoshu    string `gorm:"not null;DEFAULT:0"`
 }
 
 func (Juese5s) TableName() string {
--- qidongqian/Jianbiao.go
+++ qidongqian/Jianbiao.go
@@ -9,16 +9,8 @@ func Chuangjianbiao() {
 	dbs := quanju.Db().Set("gorm:table_options", "ENGINE=InnoDB")
 	defer dbs.Close()
 	dbs.CreateTable(
-		&moxings.Gl1he2s{},
-		&moxings.Gl2he4s{},
-		&moxings.Gl1he5s{},
-		&moxings.Gl5he6s{},
-		&moxings.Juese5s{},
-		&moxings.Yanzheng4s{},
-		&moxings.Zhanghao1s{},
-		&moxings.Zhanghaolei2s{},
-		&moxings.Zhanghaoxinxi3s{},
-		&moxings.Ziduan7s{},
-		&moxings.Ziyuan6s{},
+		&moxings.Gl1he8s{},
+		&moxings.Lingpai8s{},
+		&moxings.Lingpailei9s{},
 	)
 }
