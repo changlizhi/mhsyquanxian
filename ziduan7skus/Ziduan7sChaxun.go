57957f68ca6e6d677192aae18f80eb6ffacee6cc
changlz <changlizhi@aliyun.com>
2017-12-19 14:00:35 +0800

    增加查询全部的方法和测试

--- ziduan7skus/Ziduan7sChaxun.go
+++ ziduan7skus/Ziduan7sChaxun.go
@@ -5,7 +5,7 @@ import (
 	"mhsyquanxian/moxings"
 	"mhsyquanxian/quanju"
 )
-
+//返回mx中条件的数据，若mx的数据都为""或0的话就返回Id最大的一条记录
 func ChaxunZiduan7s(mx moxings.Ziduan7s) *moxings.Ziduan7s {
 	find := quanju.Db().Find(&moxings.Ziduan7s{}, mx)
 	if find.Error != nil {
@@ -14,4 +14,9 @@ func ChaxunZiduan7s(mx moxings.Ziduan7s) *moxings.Ziduan7s {
 	}
 	ret := find.Value.(*moxings.Ziduan7s)
 	return ret
+}
+func SuoyouZiduan7s() *[]moxings.Ziduan7s {
+	mxs := []moxings.Ziduan7s{}
+	find := quanju.Db().Find(&mxs)
+	return find.Value.(*[]moxings.Ziduan7s)
 }
--- ziduan7skusceshis/Ziduan7sChaxun_test.go
+++ ziduan7skusceshis/Ziduan7sChaxun_test.go
@@ -11,3 +11,8 @@ func TestChaxunZiduan7s(t *testing.T) {
 	ret := ziduan7skus.ChaxunZiduan7s(ceshiyong.Ziduan7schaxun())
 	log.Println(ret)
 }
+
+func TestSuoyouZiduan7s(t *testing.T) {
+	mxs := ziduan7skus.SuoyouZiduan7s()
+	log.Println(mxs)
+}
